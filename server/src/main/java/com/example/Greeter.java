67f24bf84a3ef8e68c243c18ac331ce14fc1ccbb
Jesus Racionero Serna <jracione@yahoo.es>
2018-02-18 10:40:29 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,8 +13,8 @@ public class Greeter {
   }
 
 /**
-/* @param Jesus RS
-/* @return greeting string
+* @param Jesus RS
+* @return greeting string
 */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
